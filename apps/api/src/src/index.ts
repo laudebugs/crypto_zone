04ef149f1b2448fc1f81aa92b784228d97a70883
lbugasu <lbugasu@gmail.com>
2021-04-13 16:54:03 -0400

    adding logs

--- apps/api/src/src/index.ts
+++ apps/api/src/src/index.ts
@@ -7,7 +7,4 @@ import { scheduleSnapShots } from "./graphql/workers/snapshotWorker";
 // Connect to mongo database
 require("./database/mongo");
 
-// Start the server
-require("./server/server");
-
 scheduleSnapShots();
